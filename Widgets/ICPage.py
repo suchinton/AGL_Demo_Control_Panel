1660cf347b54fbe1e0724bd55cdc8addfde65138
Suchinton <75079303+suchinton@users.noreply.github.com>
2023-07-03 14:50:25 +0530

    IC Page update

--- Widgets/ICPage.py
+++ Widgets/ICPage.py
@@ -57,6 +57,8 @@ class ICWidget(Base, Form):
         # self.setStyleSheet(theme.read())
         # theme.close()
 
+        self.scriptBtn = self.findChild(QPushButton, "scriptBtn")
+
         self.Speed_slider = self.findChild(QSlider, "Speed_slider")
         self.Speed_monitor = self.findChild(QLCDNumber, "Speed_monitor")
         self.RPM_slider = self.findChild(QSlider, "RPM_slider")
@@ -86,7 +88,7 @@ class ICWidget(Base, Form):
 
         self.driveGroupBtns.buttonClicked.connect(self.driveBtnClicked)
 
-        #self.reconnectBtn.clicked.connect(self.set_instance)
+        self.scriptBtn.clicked.connect(self.scriptBtnClicked)
 
         self.Speed_slider.valueChanged.connect(self.update_Speed_monitor)
         self.Speed_slider.setMinimum(0)
@@ -99,7 +101,8 @@ class ICWidget(Base, Form):
         self.coolantTemp_slider.valueChanged.connect(self.update_coolantTemp_monitor)
         self.fuelLevel_slider.valueChanged.connect(self.update_fuelLevel_monitor)
 
-        self.accelerationBtn.clicked.connect(self.accelerationBtnClicked)
+        self.accelerationBtn.pressed.connect(self.accelerationBtnPressed)
+        self.accelerationBtn.released.connect(self.accelerationBtnReleased)
 
         # make both buttons checkable
         self.leftIndicatorBtn.setCheckable(True)
@@ -127,8 +130,13 @@ class ICWidget(Base, Form):
     def set_instance(self):
         self.kuksa = kuksa_instance.KuksaClientSingleton.get_instance()
         self.client = self.kuksa.get_client()
-        # if self.client is None:
-        #     print("Client is None")
+
+    def scriptBtnClicked(self):
+        if self.scriptBtn.isChecked():
+            ICScript.start_script()
+        
+        if not self.ScriptBtn.isChecked():
+            ICScript.stop_script()
 
     def update_Speed_monitor(self):
         speed = int(self.Speed_slider.value())
@@ -190,7 +198,7 @@ class ICWidget(Base, Form):
 
             painter = QPainter(leftIndicatorIcon)
             painter.setCompositionMode(QPainter.CompositionMode_SourceIn)
-            painter.fillRect(leftIndicatorIcon.rect(), QtCore.Qt.green)
+            painter.fillRect(leftIndicatorIcon.rect(), QtCore.Qt.green)         
             painter.end()
 
             self.leftIndicatorBtn.setIcon(QIcon(leftIndicatorIcon))
@@ -225,61 +233,42 @@ class ICWidget(Base, Form):
             self.rightIndicatorBtn.setIcon(QIcon(rightIndicatorIcon))
             self.client.setValue(self.IC.rightIndicator, "false")
 
-    def calculate_speed(self, time):
-        acceleration = 60 / 5 # acceleration from 0 to 60 in 5 seconds
-        time = time / 1000 # convert milliseconds to seconds
-        speed = acceleration * time # calculate speed
-        return speed
-
-    def calculate_engine_rpm(self,speed):
-        wheel_diameter = 0.48 # in meters
-        wheel_circumference = wheel_diameter * 3.14 # in meters
-
-        gear_ratios = [3.36, 2.10, 1.48, 1.16, 0.95, 0.81]
-        speed = speed * 1000 / 3600 # Convert speed from km/h to m/s
-        wheel_rps = speed / wheel_circumference
-
-        current_gear = None
-        for i in range(len(gear_ratios)):
-            if wheel_rps * gear_ratios[i] < 8000 / 60:
-                current_gear = i + 1
-                break
-
-        # If no gear is found, use the highest gear
-        if current_gear is None:
-            current_gear = len(gear_ratios)
-
-        engine_rpm = wheel_rps * gear_ratios[current_gear - 1] * 60
-
-        return engine_rpm
-
-    def calculate_speed_from_rpm_and_gear(self,rpm,gear):
-
-        wheel_diameter = 0.48 # in meters
-        wheel_circumference = wheel_diameter * 3.14 # in meters
-
-        gear_ratios = [3.36, 2.10, 1.48, 1.16, 0.95, 0.81]
-        wheel_rps = rpm / (gear_ratios[gear - 1] * 60)
-
-        # Calculate the speed in m/s
-        speed = wheel_rps * wheel_circumference
-        speed = speed * 3600 / 1000
-
-        return speed
-
-    def accelerationBtnClicked(self):
+    def accelerationBtnPressed(self):
         self.startTime = QtCore.QTime.currentTime()
-        self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.updateSpeedAndEngineRpm)
-        self.timer.start(100)
+        self.acceleration_timer = QtCore.QTimer()
+        self.acceleration_timer.timeout.connect(lambda: self.updateSpeedAndEngineRpm("Accelerate"))
+        self.acceleration_timer.start(100)
 
-    def updateSpeedAndEngineRpm(self):
+    def accelerationBtnReleased(self):
+        if self.Speed_slider.value() <= 0:
+            self.acceleration_timer.stop()
+        else:
+            self.acceleration_timer.timeout.connect(lambda: self.updateSpeedAndEngineRpm("Decelerate"))
+            self.acceleration_timer.start(100)
+
+    def updateSpeedAndEngineRpm(self, action, acceleration=(60/5)):
+        if action == "Accelerate":
+            pass
+        elif action == "Decelerate":
+            acceleration = -acceleration
+
         currentTime = QtCore.QTime.currentTime()
         duration = self.startTime.msecsTo(currentTime)
-        speed = self.calculate_speed(duration)
-        rpm = self.calculate_engine_rpm(speed)
-        self.Speed_slider.setValue(int(speed))
-        self.RPM_slider.setValue(int(rpm))
+        self.current_speed = AccelerationFns.calculate_speed(duration, acceleration)
+        self.current_rpm = AccelerationFns.calculate_engine_rpm(self.current_speed)
+
+        if self.current_speed <= 0:
+            self.current_speed = 0
+            self.current_rpm = 0
+            self.acceleration_timer.stop()
+
+        if self.current_speed >= 240:
+            self.current_speed = 240
+            self.current_rpm = 0
+            self.acceleration_timer.stop()
+
+        self.Speed_slider.setValue(self.current_speed)
+        self.RPM_slider.setValue(self.current_rpm)
         self.send_value()
 
     def driveBtnClicked(self):
@@ -309,6 +298,66 @@ class ICWidget(Base, Form):
             self.Speed_slider.setEnabled(False)
             self.RPM_slider.setEnabled(True)
             self.client.setValue(self.IC.selectedGear, "0")
+
+
+class AccelerationFns():
+    def calculate_speed(time, acceleration) -> int:
+        #acceleration = 60 / 5 # acceleration from 0 to 60 in 5 seconds
+        time = time / 1000 # convert milliseconds to seconds
+        speed = acceleration * time # calculate speed
+        return int(speed)
+
+    def calculate_engine_rpm(speed) -> int:
+        wheel_diameter = 0.48 # in meters
+        wheel_circumference = wheel_diameter * 3.14 # in meters
+
+        # Adjust the gear ratios to match the desired speed and rpm
+        gear_ratios = [3.36, 2.10, 1.48, 1.16, 0.95, 0.75]
+        speed = speed * 1000 / 3600 # Convert speed from km/h to m/s
+        wheel_rps = speed / wheel_circumference
+
+        current_gear = None
+        for i in range(len(gear_ratios)):
+            if wheel_rps * gear_ratios[i] < 8000 / 60:
+                current_gear = i + 1
+                break
+
+        # If no gear is found, use the highest gear
+        if current_gear is None:
+            current_gear = len(gear_ratios)
+
+        engine_rpm = wheel_rps * gear_ratios[current_gear - 1] * 60
+
+        return int(engine_rpm)
+
+# create a class ICScript and inherit from the ICWidget class
+class ICScript(ICWidget):
+    def start_script(self):
+        ICWidget.reset()
+
+        # disable all widgets in the scroll area
+        for widget in ICWidget.scrollAreaWidgetContents.children():
+            widget.setEnabled(False)
+
+        rates = [(60/5), (60/4), (60/3)]
+
+        # start assigning values to the  speed and rpm sliders and send them to the IC do this in a loop for each rate
+        for rate in rates:
+            ICWidget.accelerationBtnPressed()
+            ICWidget.acceleration_timer.timeout.connect(lambda: ICWidget.updateSpeedAndEngineRpm("Accelerate"), rate)
+            ICWidget.acceleration_timer.start(100)
+            time.sleep(5)
+            ICWidget.accelerationBtnReleased()
+            ICWidget.acceleration_timer.timeout.connect(lambda: ICWidget.updateSpeedAndEngineRpm("Decelerate"), rate)
+            ICWidget.acceleration_timer.start(100)
+            time.sleep(5)
+
+    def stop_script(self):
+        ICWidget.reset()
+
+        # enable all widgets in the scroll area
+        for widget in ICWidget.scrollAreaWidgetContents.children():
+            widget.setEnabled(True)
 
 if __name__ == '__main__':
     import sys
