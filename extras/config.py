2ffc884a4601bca43e95c0b637823b8bc4876472
Suchinton <75079303+suchinton@users.noreply.github.com>
2023-07-05 01:18:14 +0530

    Fix static python version in JWT token path

--- extras/config.py
+++ extras/config.py
@@ -1,4 +1,7 @@
 import os
+import platform
+
+python_version = "python" + platform.python_version_tuple()[0] + "." + platform.python_version_tuple()[1]
 
 KUKSA_CONFIG = {
     "ip": '10.10.10.203',
@@ -7,9 +10,5 @@ KUKSA_CONFIG = {
     'insecure': True,
 }
 
-# fetch token from file from .local/lib/python3.10/site-packages/kuksa_certificates/jwt/super-admin.json.token
-
 TOKEN_PATH = os.path.join(os.path.expanduser("~"), 
-                          ".local/lib/python3.10/site-packages/kuksa_certificates/jwt/super-admin.json.token")
-
-print(TOKEN_PATH)
+                          f".local/lib/{python_version}/site-packages/kuksa_certificates/jwt/super-admin.json.token")
