96ab34ce67b578ac73c48a482c8ac2adbfdccd71
Suchinton <75079303+suchinton@users.noreply.github.com>
2023-07-07 10:28:52 +0530

    Update default settings

--- extras/config.py
+++ extras/config.py
@@ -4,10 +4,10 @@ import platform
 python_version = "python" + platform.python_version_tuple()[0] + "." + platform.python_version_tuple()[1]
 
 KUKSA_CONFIG = {
-    "ip": '10.10.10.203',
+    "ip": 'localhost',
     "port": "8090",
     'protocol': 'ws',
-    'insecure': True,
+    'insecure': False,
 }
 
 TOKEN_PATH = os.path.join(os.path.expanduser("~"), 
