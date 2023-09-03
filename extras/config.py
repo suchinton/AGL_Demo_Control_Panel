a9e9ebf645e7a74c3a38fffc9568da7e8788821c
suchinton <75079303+suchinton@users.noreply.github.com>
2023-09-04 02:01:36 +0530

    Add CA.pem to config
    - Enable Secure connection to kuksa-val-server
    - Add AGL's CA.pem as asset

--- extras/config.py
+++ extras/config.py
@@ -1,13 +1,17 @@
 import os
 import platform
 
-python_version = "python" + platform.python_version_tuple()[0] + "." + platform.python_version_tuple()[1]
+python_version = f"python{'.'.join(platform.python_version_tuple()[:2])}"
+
+CA = os.path.abspath(os.path.join(os.path.dirname(__file__), "../assets/CA.pem"))
 
 KUKSA_CONFIG = {
     "ip": 'localhost',
     "port": "8090",
     'protocol': 'ws',
     'insecure': False,
+    'cacertificate': CA,
+    'tls_server_name': "Server",
 }
 
 TOKEN_PATH = os.path.join(os.path.expanduser("~"), 
